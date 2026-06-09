7ae40e9d419a6391c5270f4563926a0cbe4b2aae
hDanford <hdanford01@gmail.com>
2026-06-09 18:56:03 -0400

    Increase LOCATION_DECAY_KM to 10000

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 const MAX_LOCATION_SCORE    = 5000;
 const MAX_STATUS_SCORE      = 5000;
 const MAX_TOTAL             = 10000;
-const LOCATION_DECAY_KM     = 2000;  // km at which location score reaches ~0
+const LOCATION_DECAY_KM     = 10000;  // km at which location score reaches ~0
 const STATUS_POINTS_PER_STEP = 1000; // lose 1000 per IUCN step off
 const STORAGE_KEY           = "rangeguessr_v2_history";
 
